89596a5e1a7de65baa6d2e463bab7a45c46652f9
JPGIODevs <jpgiodevelopments@gmail.com>
2023-06-05 11:19:05 +0100

    cart additions

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,8 @@
 class Cart < ApplicationRecord
-end
+  has_many :orderables
+  has_many :products, through: :orderables
+
+  def total
+    orderables.to_a.sum{ |orderable| orderable.total }
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :products
  
-  get 'cart', to 'cart#show'
+  get 'cart', to: 'cart#show'
+  post 'cart/add'
+  post 'cart/remove'
+
 
   get 'site/index'
   get 'items/index'
